9c9ed7541722669fed0cd1940d73ea5f81a19685
Austin Carter <austin.carter.1@und.edu>
2026-02-03 18:33:00 -0500

    Create Prog2part4.py

---
+++ Prog2part4.py
@@ -0,0 +1,15 @@
+# Ask for number of completed credits
+credits = int(input("Number of completed credits: "))
+
+# Determine class rank
+if credits <= 23:
+    rank = "Freshman"
+elif credits <= 59:
+    rank = "Sophomore"
+elif credits <= 89:
+    rank = "Junior"
+else:
+    rank = "Senior"
+
+# Output result
+print("Class rank:", rank)
