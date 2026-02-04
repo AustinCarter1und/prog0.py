b5e2202120ab1aafa0e1e15520862d8fcc26710a
Austin Carter <austin.carter.1@und.edu>
2026-02-03 19:39:38 -0500

    Revise author details and streamline result display
    Updated author information and simplified output.

--- Prog2part3.py
+++ Prog2part3.py
@@ -1,13 +1,10 @@
-# Prog2part3.py
-# Author: Austin Carter
+# Name: Austin Carter
 # Email: austin.carter.1@und.edu
 
-# Ask for numbers and operator
 num1 = float(input("Enter the first number: "))
 op = input("Enter the operation: ")
 num2 = float(input("Enter the second number: "))
 
-# Perform the operation
 if op == "+":
     result = num1 + num2
 elif op == "-":
@@ -18,11 +15,9 @@ elif op == "/":
     result = num1 / num2
 elif op == "//":
     result = num1 // num2
-else:
-    result = "Invalid operator"
 
-# Display result
-print("The result is:", result)
+print(result)
+
 
 
 
