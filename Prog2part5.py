6f10621a544004fbc804efbad402e161c87c40d3
Austin Carter <austin.carter.1@und.edu>
2026-02-03 19:43:09 -0500

    Refactor author info and streamline output
    Updated author information and simplified output.

--- Prog2part5.py
+++ Prog2part5.py
@@ -1,15 +1,13 @@
-# Prog2part5.py
-# Author: Austin Carter
+# Name: Austin Carter
 # Email: austin.carter.1@und.edu
 
 value = int(input("Enter an integer value: "))
 
-# Check if the value is within the 400s
 if 400 <= value <= 499:
     answer = "yes"
 else:
     answer = "no"
 
-# Output result
-print("Within range?", answer)
+print(answer)
+
 
