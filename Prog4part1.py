c19661baad0abad91426c664ae10624c29e89186
Austin Carter <austin.carter.1@und.edu>
2026-02-15 16:17:15 -0600

    Add factorial calculation script

---
+++ Prog4part1.py
@@ -0,0 +1,11 @@
+# name: Austin Carter
+# email: austin.carter.1@und.edu
+
+value = int(input("Enter value: "))
+
+factorial = 1
+for i in range(1, value + 1):
+    factorial *= i
+
+print("Factorial:", factorial)
+
